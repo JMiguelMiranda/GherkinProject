d9dec3f6a016b2b7ef442c5a682feeb0c1171e65
Damian Duarte <damian.duarte@tiendamia.com>
2022-07-19 11:14:45 -0300

    Mini fix

--- cypress/integration/normal-script/product/product.js
+++ cypress/integration/normal-script/product/product.js
@@ -8,31 +8,23 @@ const wrtUrl = '/product/wrt/633860434';
 const mcyUrl = '/product/mcy/8570453';
 
 Given('A user opens an Amazon product', ()=>{
-    cy.visit(amzUrl).then(() => {
-        cy.url(amzUrl).should('include', amzUrl);
-        cy.wait(3000);
-    });
+    cy.visit(amzUrl);
+    cy.wait(4000);
 })
 
 Given('A user opens an eBay product', ()=>{
-    cy.visit(ebyUrl).then(() => {
-        cy.url(ebyUrl).should('include', ebyUrl);
-        cy.wait(3000);
-    });
+    cy.visit(ebyUrl);
+    cy.wait(4000);
 })
 
 Given('A user opens an Walmart product', ()=>{
-    cy.visit(wrtUrl).then(() => {
-        cy.url(wrtUrl).should('include', wrtUrl);
-        cy.wait(3000);
-    });
+    cy.visit(wrtUrl);
+    cy.wait(4000);
 })
 
 Given('A user opens an Macy\'s product', ()=>{
-    cy.visit(mcyUrl).then(() => {
-        cy.url(mcyUrl).should('include', mcyUrl);
-        cy.wait(3000);
-    });
+    cy.visit(mcyUrl);
+    cy.wait(4000);
 })
 
 When('A user validates product information', () => {
